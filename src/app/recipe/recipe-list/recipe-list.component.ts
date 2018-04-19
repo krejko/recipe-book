cfe0c63abc974f7b8d167330781e0059ac526d5c
Kevin Rejko <krejko@gmail.com>
2018-04-19 12:51:36 -0400

    Lecture 80: Added ability to select recipe from list and display in recipe detailg

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from "../recipe.model"
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from "../recipe.model"
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
     new Recipe('Cruncy Bacon', 'It\'s delicious!', 'https://assets.epicurious.com/photos/57714624e43289453ac28e41/master/pass/diner-bacon-hero-22062016.jpg'),
@@ -18,5 +19,9 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeSelected.emit(recipe);
+  } 
 
 }
