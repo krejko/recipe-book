c96db9abc47e4956305354936a5ced80b18b4ca6
Kevin Rejko <krejko@gmail.com>
2018-04-19 13:23:53 -0400

    Lecture 81: Added ability to add ingredients to shopping list

--- src/app/shopping/shopping-list-editor/shopping-list-editor.component.ts
+++ src/app/shopping/shopping-list-editor/shopping-list-editor.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model'
 
 @Component({
   selector: 'app-shopping-list-editor',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list-editor.component.css']
 })
 export class ShoppingListEditorComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAddItem(){
+    const name = this.nameInputRef.nativeElement.value;
+    const amount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(name, amount); 
+    this.ingredientAdded.emit(newIngredient);
+   }
 }
--- src/app/shopping/shopping-list/shopping-list.component.ts
+++ src/app/shopping/shopping-list/shopping-list.component.ts
@@ -18,4 +18,7 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
   }
 
+  addedIngredient(ingredient: Ingredient){
+    this.ingredients.push(ingredient);
+  }
 }
