781e7c20e574524ed41209fa28e97b8b5794f21c
Kevin Rejko <krejko@gmail.com>
2018-04-20 12:55:56 -0400

    Removed non-router code from header and app module

--- src/app/routes.module.ts
+++ src/app/routes.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router'
 
 import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
@@ -13,7 +12,6 @@ const appRoutes: Routes = [
 
 @NgModule({
   imports: [
-    CommonModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [
